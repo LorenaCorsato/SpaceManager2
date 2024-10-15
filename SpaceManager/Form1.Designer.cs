8627bd4e6fa5d48d65d803adc7a8b367fdec777d
Lyncon224 <lynconoliveira9710@gmail.com>
2024-10-15 19:44:33 -0300

    kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk

--- SpaceManager/Form1.Designer.cs
+++ SpaceManager/Form1.Designer.cs
@@ -174,7 +174,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(857, 174);
+            this.label4.Location = new System.Drawing.Point(894, 226);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(44, 16);
             this.label4.TabIndex = 5;
